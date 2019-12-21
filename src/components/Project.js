8e6bfcd379244f40a2fef169e5d940c3bcd9b403
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-12-21 10:47:45 -0800

    added basic component

---
+++ src/components/Project.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Project extends React.Component {
+
+    render() {
+        return (
+            <h1>Project</h1>
+        )
+    }
+
+}
+
+export default Project;
