589596277d6be444e9a32e51fa9829ff59207ab3
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-12-28 22:44:58 -0800

    added basic styling for home section

--- src/components/sections/home/HomeSection.js
+++ src/components/sections/home/HomeSection.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Name from './Name';
+import Details from './Details';
 
 class HomeSection extends React.Component {
 
     render() {
         return (
             <div id="home" className="section">
-                <h2>Intro</h2>
+                <Name />
+                <Details />
             </div>
         )
     }
---
+++ src/components/sections/home/Name.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Name() {
+    return (
+        <div>
+            <h1 id="name">Jesus R Mendoza</h1>
+        </div>
+    )
+}
+
+export default Name;
