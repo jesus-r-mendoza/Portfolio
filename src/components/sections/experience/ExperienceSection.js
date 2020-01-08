254dd0220332d39a281995cc83d109b1f2559a24
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2020-01-07 19:10:07 -0800

    added skeleton for columns of summaries

--- src/components/sections/experience/ExperienceSection.js
+++ src/components/sections/experience/ExperienceSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Overview from './Overview';
+import Summaries from './Summaries';
 
 class ExperienceSection extends React.Component {
 
@@ -8,6 +9,7 @@ class ExperienceSection extends React.Component {
             <div id="experience" className="section">
                 <h2>Academic Internship</h2>
                 <Overview />
+                <Summaries />
             </div>
         )
     }
