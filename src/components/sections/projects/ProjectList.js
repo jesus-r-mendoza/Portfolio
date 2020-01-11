ef029017cdd244a667720a5f72c710f1dcd26157
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2020-01-11 15:27:37 -0800

    properly fetching all necessary project data from api

--- src/components/sections/projects/ProjectList.js
+++ src/components/sections/projects/ProjectList.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Project from './Project';
 
+const repoAPI = 'https://api.github.com/repos/jesus-r-mendoza/';
+const statsAPI = '/stats/contributors';
+
 class ProjectList extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            pinnedRepos: [
+                'Operations-Data-and-Mgmt-System',
+                'State-Farm-Distracted-Driver-Detection',
+                'Notely',
+                'Walmart-Store-Sales-Forecasting',
+                'OS-File-Management-System',
+                'RSA-Encryption'
+            ],
             error: null,
             isLoaded: false,
             projects: [
@@ -38,11 +49,11 @@ class ProjectList extends React.Component {
     }
 
     componentDidMount() {
-        fetch('https://api.github.com/users/jesus-r-mendoza/repos?sorted=created')
+        fetch(repoAPI + 'Operations-Data-and-Mgmt-System')
         .then(res => res.json())
         .then(
             (result) => {
-                console.log(result);
+                getDataFromRepo(result);
             },
             (error) => {
                 console.log(error)
@@ -58,6 +69,35 @@ class ProjectList extends React.Component {
         )
     }
 
+}
+
+function getDataFromRepo(response) {
+    console.log('--------------------------------------');
+    console.log(response.name);
+    console.log(response.updated_at);
+    if (!response.language) {
+        try {
+            console.log(response.parent.language);
+        } catch(e) {
+            console.log('View repo for more details');
+        }
+    } else {
+        console.log(response.language);
+    }
+    fetch(repoAPI + 'Operations-Data-and-Mgmt-System' + statsAPI)
+    .then(res => res.json())
+    .then(
+        (result) => {
+            console.log(result);
+            console.log(result.length);
+            let x = result.reduce((sum, contributor) => sum + contributor.total, 0);
+            console.log(x);
+        },
+        (error) => {
+            console.log(error);
+        }
+    );
+    console.log('--------------------------------------');
 }
 
 export default ProjectList;
