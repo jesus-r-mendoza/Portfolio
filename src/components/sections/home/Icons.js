05475aab35997e99aa5ef88c99c55cd8332027ec
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2020-01-12 17:18:50 -0800

    removed comments and added fontawesome cdn link

--- src/components/sections/home/Icons.js
+++ src/components/sections/home/Icons.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Icons() {
     return (
         <div className="d-flex justify-content-center my-5">
-            <div className="my-5 mx-1 d-flex justify-content-around align-items-center w-75">
+            <div className="my-5 d-flex justify-content-around align-items-center w-75">
                 <div className="icon"></div>
                 <div className="icon"></div>
                 <div className="icon"></div>
