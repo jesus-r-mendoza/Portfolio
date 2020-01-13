74f4ebfae1f77c6299e1fdec1f31d3d90a7f9afe
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2020-01-12 18:27:05 -0800

    added correct icons; icons link to info; added tooltips to icons

--- src/components/sections/home/Icons.js
+++ src/components/sections/home/Icons.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 function Icons() {
     return (
         <div className="d-flex justify-content-center my-5">
             <div className="my-5 d-flex justify-content-around align-items-center w-75">
-                <div className="icon"></div>
-                <div className="icon"></div>
-                <div className="icon"></div>
+                <OverlayTrigger placement="top"
+                    overlay={ <Tooltip>View my Github projects</Tooltip> }>
+                    <a href="https://github.com/jesus-r-mendoza" target="_blank" rel="noopener noreferrer" className="icon-link">
+                        <i className="fab fa-github-square icon"></i>
+                    </a>
+                </OverlayTrigger>
+
+                <OverlayTrigger placement="top"
+                    overlay={ <Tooltip>Contact me via Email</Tooltip> }>
+                    <a href="mailto:jesus_r_mendoza@yahoo.com" className="icon-link">
+                        <i className="far fa-envelope icon"></i>
+                    </a>
+                </OverlayTrigger>
+
+                <OverlayTrigger placement="top"
+                    overlay={ <Tooltip>Let's connect LinkedIn</Tooltip> }>
+                    <a href="https://www.linkedin.com/in/jesus-r-mendoza" target="_blank" rel="noopener noreferrer" className="icon-link">
+                        <i className="fab fa-linkedin icon"></i>
+                    </a>
+                </OverlayTrigger>
             </div>
         </div>
     )
