1aa3e10f62cd5e6d34f6a1d62c042beccea3fe48
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2020-03-08 21:10:21 -0700

    updated home section to include pic; resizes with screen width; profile pic removed when screen size < 600px wide

--- src/components/sections/home/Details.js
+++ src/components/sections/home/Details.js
@@ -2,13 +2,17 @@ import React from 'react'
 
 function Details() {
     return (
-        <div id="details" className="d-flex justify-content-between align-items-start my-5 pt-5">
-            <div className="w-50">
-                <h2 id="desc" className="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, laboriosam.</h2>
-                <h4 className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente voluptatum, doloribus fugit corrupti adipisci quam iure dignissimos, quis harum sit, qui veritatis iusto magnam officiis?</h4>
+        <div id="details">
+            <div className="intro">
+                <h2 id="desc" className="mb-4">Data Scientist, Software Engineer, Volunteer, Golfer, Passionate Programmer </h2>
+                <p className="lead">
+                    HI, I'm currently and Undergrad at Cal State LA and will be Graduating in May 2020 with a BS in CS.
+                    I’m seeking a full-time, part-time, or internship position in several branches of CS. Interests in Data Science &amp; Machine Learning,
+                    Software Engineering, and App Development.
+                </p>
             </div>
             <div>
-                <div id="img"></div>
+                <img id="profile-pic" src="https://avatars2.githubusercontent.com/u/34051207?v=4" alt=""/>
             </div>
         </div>
     )
