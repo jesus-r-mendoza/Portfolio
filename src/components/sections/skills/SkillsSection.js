3e38dff09c18aae2547a249673d1d28c40497093
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2020-03-09 10:31:39 -0700

    changed styling for skiill card which contains skill pills

--- src/components/sections/skills/SkillsSection.js
+++ src/components/sections/skills/SkillsSection.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import SkillCard from './SkillCard';
 
 class SkillsSection extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            skills: [
+                {
+                    id: 1,
+                    name: "Programming Languages",
+                    pills: [
+                        "Java", "Python", "C/C++", "JavaScript", "Kotlin",
+                        "Haskell", "Sql", "HTML", "CSS"
+                    ]
+                },
+                {
+                    id: 2,
+                    name: "Software Engineering",
+                    pills: [
+                        "Django", "React", "MySql", "Docker"
+                    ]
+                },
+                {
+                    id: 3,
+                    name: "Machine Learning",
+                    pills: [
+                        "Jupyter Notebook", "Pandas", "Numpy", "SciKitLearn",
+                        "Keras", "Matplotlib"
+                    ]
+                }
+            ],
+        };
+    }
 
     render() {
         return (
             <div id="skills" className="section">
                 <h2 className="display-4">Skills</h2>
+                <div className="d-flex justify-content-start align-items-start flex-wrap">
+                    {this.state.skills.map( (skill) => <SkillCard skill={skill} /> )}
+                </div>
             </div>
         )
     }
