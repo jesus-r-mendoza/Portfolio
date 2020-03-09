a1ccdd87cb9d65f92d193c254ce6b360466daef4
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2020-03-09 09:10:10 -0700

    added icon to github repos link

--- src/components/sections/projects/ProjectSection.js
+++ src/components/sections/projects/ProjectSection.js
@@ -10,7 +10,10 @@ class ProjectSection extends React.Component {
                 <ProjectList />
                 <div className="d-flex justify-content-center my-5">
                     <a href="https://github.com/jesus-r-mendoza?tab=repositories" target="_blank" rel="noopener noreferrer"
-                        className="btn btn-lg btn-outline-info">View more projects on my Github ></a>
+                        className="btn btn-lg btn-outline-info">
+                            View more projects on my Github
+                            <i class="fas fa-chevron-right ml-2"></i>
+                        </a>
                 </div>
             </div>
         )
