50e240c094f41f05bff958e31a93d57d5cac8522
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2020-03-09 10:30:19 -0700

    created initial skills card layout

---
+++ src/components/sections/skills/SkillCard.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import SkillPill from './SkillPill';
+
+class SkillCard extends Component {
+
+    render() {
+        return (
+            <div className="skill-card">
+                <h4>{this.props.skill.name}</h4>
+                <div className="pills mt-3">
+                    {this.props.skill.pills.map( (p) => <SkillPill key={p.id} pill={p} /> )}
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default SkillCard;
