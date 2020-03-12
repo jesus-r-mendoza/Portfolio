aaff2f3b57fc476e657e1c31dda3fddc92867c8b
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2020-03-11 18:27:41 -0700

    Updates

---
+++ precache-manifest.6636274cb59426b39dcd31ef10348e06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf32b80481cb0ad0cb86eb0c3ed63ae2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d60e5c230b42f303254c",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d34fa038b8196dd86fb9",
+    "url": "/static/css/main.b9a91e0c.chunk.css"
+  },
+  {
+    "revision": "d60e5c230b42f303254c",
+    "url": "/static/js/2.053a9d70.chunk.js"
+  },
+  {
+    "revision": "801eb6b718cfeb49e4699ab80a85e8cf",
+    "url": "/static/js/2.053a9d70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d34fa038b8196dd86fb9",
+    "url": "/static/js/main.495bc909.chunk.js"
+  },
+  {
+    "revision": "149642c2b69f86080224",
+    "url": "/static/js/runtime-main.c0fd5858.js"
+  },
+  {
+    "revision": "946e64e80bc4c0a4e289934b94a25342",
+    "url": "/static/media/mhs-sm.946e64e8.png"
+  }
+]);
